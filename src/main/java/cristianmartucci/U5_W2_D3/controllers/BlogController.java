16d7f0a0c5e7325e970464ab58832e155ce32ef0
CristianMartu <martuccicristian18@gmail.com>
2024-06-26 16:00:01 +0200

    Initial commit

---
+++ src/main/java/cristianmartucci/U5_W2_D3/controllers/BlogController.java
@@ -0,0 +1,43 @@
+package cristianmartucci.U5_W2_D3.controllers;
+
+import cristianmartucci.U5_W2_D3.entities.Blog;
+import cristianmartucci.U5_W2_D3.services.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/blogs")
+public class BlogController {
+    @Autowired
+    private BlogService blogService;
+
+    @GetMapping
+    private Page<Blog> getAllBlog(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "id") String sortBy) {
+        return this.blogService.getAllBlog(page, size, sortBy);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private Blog saveBlog(@RequestBody Blog blog){
+        return this.blogService.saveBlog(blog);
+    }
+
+    @GetMapping("/{blogId}")
+    private Blog findById(@PathVariable UUID blogId){
+        return this.blogService.findById(blogId);
+    }
+
+    @PutMapping("/{blogId}")
+    private Blog updateBlog(@PathVariable UUID blogId, @RequestBody Blog blog){
+        return this.blogService.updateBlog(blogId, blog);
+    }
+
+    @DeleteMapping("/{blogId}")
+    private void deleteBlog(@PathVariable UUID blogId){
+        this.blogService.deleteBlog(blogId);
+    }
+}
---
+++ src/main/java/cristianmartucci/U5_W2_D3/services/BlogService.java
@@ -0,0 +1,49 @@
+package cristianmartucci.U5_W2_D3.services;
+
+import cristianmartucci.U5_W2_D3.entities.Blog;
+import cristianmartucci.U5_W2_D3.exceptions.NotFoundException;
+import cristianmartucci.U5_W2_D3.repositories.BlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class BlogService {
+    @Autowired
+    public BlogRepository blogRepository;
+
+    public Page<Blog> getAllBlog(int pageNumber, int pageSize, String sortBy) {
+        if (pageSize > 100) pageSize = 100;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(sortBy));
+        return blogRepository.findAll(pageable);
+    }
+
+    public Blog saveBlog(Blog blog){
+        
+        return this.blogRepository.save(blog);
+    }
+
+    public Blog findById(UUID id){
+        return this.blogRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+    }
+
+    public Blog updateBlog(UUID id, Blog updateBlog){
+        Blog blog = this.findById(id);
+        blog.setCategoria(updateBlog.getCategoria());
+        blog.setTitolo(updateBlog.getTitolo());
+        blog.setCover(updateBlog.getCover());
+        blog.setContenuto(updateBlog.getContenuto());
+        blog.setTempoDiLettura(updateBlog.getTempoDiLettura());
+        return this.blogRepository.save(blog);
+    }
+
+    public void deleteBlog(UUID id){
+        this.blogRepository.delete(this.findById(id));
+    }
+
+}
